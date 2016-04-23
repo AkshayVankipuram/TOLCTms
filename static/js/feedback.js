3d5c03bfdc4da39f6410578276ac87b702bc41c9
Ajith <23ajith7@gmail.com>
2016-04-23 11:24:22 -0700

    Js file for the feedback page (peer evaluation)

---
+++ static/js/feedback.js
@@ -0,0 +1,68 @@
+$(function() {
+
+	$('.generalMetricRadio').change(function(){
+		$('#generalMetricScoreDiv').html(computeScore('generalMetricEvaluationTable'));
+	});
+	$('.taskMetricRadio').change(function(){
+		$('#taskMetricScoreDiv').html(computeScore('taskMetricEvaluationTable'));
+	});
+
+    $('#submitButton').click(function() {
+
+		if($('#peerListDiv').find('.active').length == 0)
+			alert("Please select a Peer for evaluation.");
+		else
+		{
+			var currentPeer = $('#peerListDiv').find('.active')[0];
+			alert("Your evaluation for " + currentPeer.innerHTML+" has been recorded. Thank you.");
+			currentPeer.remove();
+			resetForm();
+			
+			if($('#peerListDiv').find('a').length == 0)
+				window.location.href = '/home'
+		}
+    });
+	
+	$('#peerListDiv a').click(function(e) {
+        e.preventDefault()
+        $that = $(this);
+        $('#peerListDiv').find('a').removeClass('active');
+        $that.addClass('active');
+    });
+});
+
+
+function computeScore(className)
+{
+	var selectedVal = "";
+	var selected = $("."+className+" label.active");
+	var count = 0;
+	var score = 0.0;
+	if (selected.length > 0) {
+		selected.each(function(){
+			if($(this).find('input')[0].value != 'na')
+			{	
+				score+= parseInt($(this).find('input')[0].value);
+				count++
+			}
+		});
+	}
+	if(count > 0)
+		return (score/count).toFixed(2);
+	else 
+		return 'NA';
+}
+
+function resetForm()
+{
+	$(".generalMetricEvaluationTable label.active").removeClass('active');
+	$(".taskMetricEvaluationTable label.active").removeClass('active');
+	
+	var naGeneralLabels = $(".generalMetricEvaluationTable label").find('input[value="na"]').parent();
+	naGeneralLabels.each(function(){$(this).addClass('active');});
+	var naTaskLabels = $(".taskMetricEvaluationTable label").find('input[value="na"]').parent();
+	naTaskLabels.each(function(){$(this).addClass('active');});
+	
+	$('#generalMetricScoreDiv').html('NA');
+	$('#taskMetricScoreDiv').html('NA');
+}
